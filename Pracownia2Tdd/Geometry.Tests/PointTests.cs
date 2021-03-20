463a57b15e75e8b7a78e64366182b345b9ad1749
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-03-20 17:18:57 +0100

    Zrobione

---
+++ Pracownia2Tdd/Geometry.Tests/PointTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Geometry.Tests
+{
+    public class PointTests
+    {
+        [Fact]
+        public void Can_create_point()
+        {
+            var point = new Point(1, 2);
+            var point2 = new Point(3.5, 2.4);
+
+            point.X.Should().Be(1);
+            point.Y.Should().Be(2);
+
+            point2.X.Should().Be(3.5);
+            point2.Y.Should().Be(2.4);
+        }
+
+        [Fact]
+        public void Can_create_point_without_parameters()
+        {
+            var point = new Point();
+
+            point.X.Should().Be(1);
+            point.Y.Should().Be(1);
+        }
+
+        [Fact]
+        public void Distance_should_be_correct()
+        {
+            var point = new Point(3, 4);
+
+            var distance = point.Distance();
+
+            distance.Should().Be(5);
+        }
+
+        [Fact]
+        public void Distance_between_points_should_be_correct()
+        {
+            var point1 = new Point(-1, 2);
+            var point2 = new Point(2, 6);
+
+            var distance = point1.Distance(point2);
+
+            distance.Should().Be(5);
+        }
+
+        [Fact]
+        public void Static_distance_between_points_should_be_correct()
+        {
+            var point1 = new Point(-1, 2);
+            var point2 = new Point(2, 6);
+
+            var distance = Point.Distance(point1, point2);
+
+            distance.Should().Be(5);
+        }
+
+        [Fact]
+        public void ToString_should_return_valid_representation()
+        {
+            var point = new Point(-2, 7);
+
+            point.ToString().Should().Be("(-2,7)");
+        }
+
+        [Fact]
+        public void ReflectionType_should_be_available()
+        {
+            var x = Point.ReflectionType.X;
+            var y = Point.ReflectionType.Y;
+            var origin = Point.ReflectionType.Origin;
+        }
+
+        [Fact]
+        public void Reflect_should_reflect_correctly()
+        {
+            var x = new Point(3, 5);
+
+            var reflection = x.Reflect(Point.ReflectionType.X);
+
+            reflection.X.Should().Be(3);
+            reflection.Y.Should().Be(-5);
+        }
+    }
+}
---
+++ Pracownia2Tdd/Geometry/Point.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Geometry
+{
+    public class Point
+    {
+        private readonly double _x;
+        private readonly double _y;
+
+        public Point(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public Point() : this(1,1) { }
+        
+        public double X => _x;
+
+        public double Y => _y;
+
+        public double Distance() => Math.Sqrt(_x * _x + _y * _y);
+
+        public double Distance(Point point1) =>
+            Math.Sqrt(Math.Pow(_x - point1._x, 2) + Math.Pow(_y - point1._y, 2));
+
+        public static double Distance(Point point1, Point point2) => point1.Distance(point2);
+
+        public override string ToString() => $"({_x},{_y})";
+
+        public enum ReflectionType
+        {
+            X,
+            Y,
+            Origin
+        }
+
+        public Point Reflect(ReflectionType reflectionType) =>
+            reflectionType switch
+            {
+                ReflectionType.X => new Point(_x, -_y),
+                ReflectionType.Y => new Point(-_x, _y),
+                ReflectionType.Origin => new Point(-_x, -_y),
+                _ => throw new ArgumentOutOfRangeException(nameof(reflectionType), reflectionType, null)
+            };
+    }
+}
