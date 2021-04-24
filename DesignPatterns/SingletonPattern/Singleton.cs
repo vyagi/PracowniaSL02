29abb922abfbafabeb5a6001e85c40c8bf63e04a
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-04-24 17:18:17 +0200

    Singleton Ready

---
+++ DesignPatterns/SingletonPattern/Singleton.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    //Singleton "idealny" - bezpieczny dla wątków i leniwy
+    
+    public class Singleton
+    {
+        public DateTime CreatedAt { get; }
+        private Singleton() => CreatedAt = DateTime.Now;
+
+        private static readonly Lazy<Singleton> Lazy = new(() => new Singleton());
+
+        public static Singleton GetInstance() => Lazy.Value;
+    }
+
+    //Prawie idealny, ale instancja utworzy się 
+    //jak tylko odwołam się w jakikolwiek sposób do klasy (typu Singleton)
+    // public class Singleton
+    // {
+    //     private static readonly Singleton Instance = new();
+    //
+    //     public DateTime CreatedAt { get; }
+    //
+    //     private Singleton() => CreatedAt = DateTime.Now;
+    //
+    //     public static Singleton GetInstance() => Instance;
+    // }
+
+    // public class Singleton
+    // {
+    //     private static object padLock = new();
+    //
+    //     private static volatile Singleton _instance;
+    //
+    //     public DateTime CreatedAt { get; }
+    //
+    //     private Singleton()
+    //     {
+    //         CreatedAt = DateTime.Now;
+    //     }
+    //
+    //     public static Singleton GetInstance()
+    //     {
+    //         if (_instance != null)
+    //             return _instance;
+    //
+    //         lock (padLock)
+    //         {
+    //             if (_instance == null)
+    //             {
+    //                 _instance = new Singleton();
+    //             }
+    //         }
+    //
+    //         return _instance;
+    //     }
+    // }
+
+
+    //Słaba efektywność
+    // public class Singleton
+    // {
+    //     private static object padLock = new object();
+    //     
+    //     private static Singleton _instance;
+    //     
+    //     public DateTime CreatedAt { get; }
+    //     
+    //     private Singleton()
+    //     {
+    //         CreatedAt = DateTime.Now;
+    //     }
+    //
+    //     public static Singleton GetInstance()
+    //     {
+    //         lock (padLock)
+    //         {
+    //             if (_instance == null)
+    //             {
+    //                 _instance = new Singleton();
+    //             }
+    //         }
+    //
+    //         return _instance;
+    //     }
+    // }
+
+
+    //Tego nie używać w środowisku wielowątkowym
+    // public class Singleton
+    // {
+    //     private static Singleton _instance;
+    //     
+    //     public DateTime CreatedAt { get; }
+    //     
+    //     private Singleton()
+    //     {
+    //         CreatedAt = DateTime.Now;
+    //     }
+    //
+    //     public static Singleton GetInstance()
+    //     {
+    //         if (_instance == null)
+    //         {
+    //             _instance = new Singleton();
+    //         }
+    //
+    //         return _instance;
+    //     }
+    // }
+}
---
+++ DesignPatterns/SingletonTests/SingletonTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using SingletonPattern;
+using Xunit;
+
+namespace SingletonTests
+{
+    public class SingletonTests
+    {
+        [Fact]
+        public void Instance_can_be_created_and_date_is_correct()
+        {
+            var s = Singleton.GetInstance();
+
+            s.Should().NotBeNull();
+            s.CreatedAt.Should().BeCloseTo(DateTime.Now, 1000);
+        }
+
+        [Fact]
+        public void There_can_be_only_one_instance()
+        {
+            var s1 = Singleton.GetInstance();
+            var s2 = Singleton.GetInstance();
+
+            ReferenceEquals(s1, s2).Should().BeTrue();
+        }
+
+        [Fact]
+        public void There_can_be_only_one_instance_in_multithreaded_environment()
+        {
+            Singleton s1 = null, s2 = null;
+
+            Task task1 = Task.Factory.StartNew(() => s1 = Singleton.GetInstance());
+            Task task2 = Task.Factory.StartNew(() => s2 = Singleton.GetInstance());
+
+            Task.WaitAll(task1, task2);
+
+            ReferenceEquals(s1, s2).Should().BeTrue();
+        }
+    }
+}
