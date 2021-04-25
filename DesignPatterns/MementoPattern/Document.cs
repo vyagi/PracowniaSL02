7e897a26febb30be1ff4714471a2c9c63fd0087f
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-04-25 19:27:04 +0200

    Memento

---
+++ DesignPatterns/MementoPattern/Document.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace MementoPattern
+{
+    public class DocumentMemento
+    {
+        private readonly string _content;
+
+        public DocumentMemento(string content) => _content = content;
+
+        public string GetContent() => _content;
+    }
+
+    public class History
+    {
+        private readonly Stack<DocumentMemento> _states = new();
+
+        public void Save(DocumentMemento documentMemento) =>
+            _states.Push(documentMemento);
+
+        public DocumentMemento Retrieve() =>
+            _states.Pop();
+    }
+
+    public class Document
+    {
+        private string _content;
+        
+        public void SetContent(string newContent) => 
+            _content = newContent;
+
+        public string GetContent() => _content;
+
+        public DocumentMemento CreateState() =>
+            new (_content);
+
+        public void Restore(DocumentMemento documentMemento) =>
+            _content = documentMemento.GetContent();
+    }
+}
---
+++ DesignPatterns/MementoPatternTests/DocumentTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentAssertions;
+using MementoPattern;
+using Xunit;
+
+namespace MementoPatternTests
+{
+    public class DocumentTests
+    {
+        [Fact]
+        public void Can_set_and_read_content()
+        {
+            var d = new Document();
+            d.SetContent("Marcin");
+
+            d.GetContent().Should().Be("Marcin");
+        }
+
+        [Fact]
+        public void Can_save_and_retrieve_state()
+        {
+            var history = new History();
+
+            var document = new Document();
+            document.SetContent("Marcin");
+
+            history.Save(document.CreateState());
+
+            document.SetContent("Jagieła");
+
+            document.Restore(history.Retrieve());
+
+            document.GetContent().Should().Be("Marcin");
+        }
+    }
+}
