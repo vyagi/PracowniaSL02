97cda8d09325ab852c32ee6b634009c11b703d63
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-04-25 17:12:18 +0200

    Fluent builder

---
+++ DesignPatterns/FluentBuilderPattern/MarineUnitBuilder.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+
+namespace FluentBuilderPattern
+{
+    public class MarineUnitBuilder : INameSetter, IIntendedUseSetter
+        , IDimensionsSetter, IMechanicalInstallationSetter, IVersatileInstallationSetter
+        , IElectricalInstallationSetter, IBrandAndModelSetter, IMarineUnitBuilder
+    {
+        private readonly MarineUnit _unit = new();
+
+        private readonly List<ElectricalInstallation> _electricalInstallations = new();
+
+        private MarineUnitBuilder() { }
+
+        public static INameSetter Initialize() => new MarineUnitBuilder();
+
+        public IIntendedUseSetter WithName(string name)
+        {
+            _unit.UnitName = name;
+            return this;
+        }
+
+        public IDimensionsSetter WithIntendedUse(UnitIntendedUse unitIntendedUse)
+        {
+            _unit.UnitIntendedUse = unitIntendedUse;
+            return this;
+        }
+
+        public IMechanicalInstallationSetter WithDimensions(Dimensions dimensions)
+        {
+            _unit.Dimensions = dimensions;
+            return this;
+        }
+
+        public IVersatileInstallationSetter WithMechanicalInstallation(MechanicalInstallation mechanicalInstallation)
+        {
+            _unit.MechanicalInstallation = mechanicalInstallation;
+            return this;
+        }
+        
+        public IElectricalInstallationSetter WithVersatileInstallation(VersatileInstallation versatileInstallation)
+        {
+            _unit.VersatileInstallation = versatileInstallation;
+            return this;
+        }
+        
+        public IElectricalInstallationSetter WithElectricalInstallation(ElectricalInstallation electricalInstallation)
+        {
+            _electricalInstallations.Add(electricalInstallation);
+            return this;
+        }
+
+        public IBrandAndModelSetter WithNoMoreElectricalInstallation()
+        {
+            _unit.ElectricalInstallation = _electricalInstallations.ToArray();
+            return this;
+        }
+
+        public IMarineUnitBuilder WithBrandAndModel(string brand, string model)
+        {
+            _unit.Brand = brand;
+            _unit.Model = model;
+            return this;
+        }
+
+        public MarineUnit Build() => _unit;
+    }
+
+    public interface IMarineUnitBuilder
+    {
+        MarineUnit Build();
+    }
+
+    public interface IBrandAndModelSetter
+    {
+        IMarineUnitBuilder WithBrandAndModel(string brand, string model);
+    }
+
+    public interface IElectricalInstallationSetter
+    {
+        IElectricalInstallationSetter WithElectricalInstallation(ElectricalInstallation electricalInstallation);
+        IBrandAndModelSetter WithNoMoreElectricalInstallation();
+    }
+
+    public interface IVersatileInstallationSetter
+    {
+        IElectricalInstallationSetter WithVersatileInstallation(VersatileInstallation versatileInstallation);
+    }
+
+    public interface IMechanicalInstallationSetter
+    {
+        IVersatileInstallationSetter WithMechanicalInstallation(MechanicalInstallation mechanicalInstallation);
+    }
+
+    public interface IDimensionsSetter
+    {
+        IMechanicalInstallationSetter WithDimensions(Dimensions dimensions);
+    }
+
+    public interface IIntendedUseSetter
+    {
+        IDimensionsSetter WithIntendedUse(UnitIntendedUse unitIntendedUse);
+    }
+
+    public interface INameSetter
+    {
+        IIntendedUseSetter WithName(string name);
+    }
+}
---
+++ DesignPatterns/MarineApplication/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FluentBuilderPattern;
+
+namespace MarineApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var unit = MarineUnitBuilder
+                    .Initialize()
+                    .WithName("Luna")
+                    .WithIntendedUse(new UnitIntendedUse(TypeOfUse.MarineCommercial, "aa", "bb"))
+                    .WithDimensions(new Dimensions(100,200,300))
+                    .WithMechanicalInstallation(new MechanicalInstallation())
+                    .WithVersatileInstallation(new VersatileInstallation())
+                    .WithElectricalInstallation(new ElectricalInstallation())
+                    .WithElectricalInstallation(new ElectricalInstallation())
+                    .WithNoMoreElectricalInstallation()
+                    .WithBrandAndModel("Volvo", "Penta")
+                    .Build();
+
+            var unit1 = MarineUnitBuilder
+                .Initialize()
+                .WithName("Moja nazwa")
+                .WithIntendedUse(new UnitIntendedUse(TypeOfUse.MarineLeisure, "", ""))
+                .WithDimensions(new Dimensions(10, 20, 30))
+                .WithMechanicalInstallation(new MechanicalInstallation())
+                .WithVersatileInstallation(new VersatileInstallation())
+                .WithNoMoreElectricalInstallation()
+                .WithBrandAndModel("", "")
+                .Build();
+
+            Console.WriteLine(unit.ElectricalInstallation.Length);
+        }
+    }
+}
