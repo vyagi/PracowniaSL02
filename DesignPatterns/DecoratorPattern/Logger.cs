35df990d7e38c33a269fd5059da200ae93a33437
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-04-25 18:12:24 +0200

    Decorator Pattern

---
+++ DesignPatterns/DecoratorPattern/Logger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace DecoratorPattern
+{
+    public interface ILogger
+    {
+        string Log(string textToLog);
+    }
+
+    public class SimpleLogger : ILogger
+    {
+        public string Log(string textToLog) => 
+            $"Information: {textToLog}";
+    }
+
+    public class TimestampingLogger : ILogger
+    {
+        private ILogger _logger;
+
+        public TimestampingLogger(ILogger logger) => _logger = logger;
+
+        public string Log(string textToLog) => 
+            $"{DateTime.Now:yyyy-MM-dd:THH:mm:ss]} {_logger.Log(textToLog)}";
+    }
+
+    public class HashingLogger : ILogger
+    {
+        private ILogger _logger;
+
+        public HashingLogger(ILogger logger) => _logger = logger;
+
+        public string Log(string textToLog) => MD5Hash.Hash.Content(_logger.Log(textToLog));
+    }
+
+    // public class Logger
+    // {
+    //     public virtual string Log(string textToLog) => $"Information: {textToLog}";
+    // }
+    //
+    // public class TimestampingLogger : Logger
+    // {
+    //     public override string Log(string textToLog) => $"{DateTime.Now:yyyy-MM-dd:THH:mm:ss]} {base.Log(textToLog)}";
+    // }
+    //
+    // public class HashingLogger : Logger
+    // {
+    //     public override string Log(string textToLog) => 
+    //         MD5Hash.Hash.Content(base.Log(textToLog));
+    // }
+    //
+    // public class TimestampingHashingLogger : Logger
+    // {
+    //     public override string Log(string textToLog) => "";
+    // }
+    //
+    // public class HashingTimestampingLogger : Logger
+    // {
+    //     public override string Log(string textToLog) => "";
+    // }
+}
---
+++ DesignPatterns/DecoratorPatternTests/LoggerTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using DecoratorPattern;
+using FluentAssertions;
+using Xunit;
+
+namespace DecoratorPatternTests
+{
+    public class LoggerTests
+    {
+        [Fact]
+        public void Simple_logger_logs_properly()
+        {
+            var logger = new SimpleLogger();
+            var log = logger.Log("Marcin");
+            log.Should().Be("Information: Marcin");
+        }
+
+        [Fact]
+        public void TimestampingLogger_logs_properly()
+        {
+            var logger = new TimestampingLogger(new SimpleLogger());
+            var log = logger.Log("Marcin");
+        
+            log.Should().StartWith("2021");
+            log.Should().EndWith("Information: Marcin");
+        }
+        
+        [Fact]
+        public void HashingLogger_logs_properly()
+        {
+            var logger = new HashingLogger(new SimpleLogger());
+            var log = logger.Log("Marcin");
+        
+            log.Should().NotContain("Marcin");
+        }
+
+        [Fact]
+        public void HashingTimestampingLogger_logs_properly()
+        {
+            var logger = new TimestampingLogger(new HashingLogger(new SimpleLogger()));
+            var log = logger.Log("Marcin");
+
+            log.Should().NotContain("Marcin");
+            log.Should().StartWith("2021");
+        }
+
+        [Fact]
+        public void TimestampingHashingLogger_logs_properly()
+        {
+            var logger = new HashingLogger(new TimestampingLogger(new SimpleLogger()));
+            var log = logger.Log("Marcin");
+
+            log.Should().NotContain("Marcin");
+            log.Should().NotStartWith("2021");
+        }
+    }
+}
